0bc30d45597edb4f2610c1d129bd6dcb1217ee31
Edin Rivera <edin.rivera@jet.com>
2019-07-07 12:01:49 -0500

    skills, salary inhancements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let employees = {};
 
-function Employee(id, name, lastName, email) {
+function Employee(id, name, lastName, email = '', skills = {}) {
   let salary = {
     basico: 0,
     bonos: 0,
@@ -12,47 +12,51 @@ function Employee(id, name, lastName, email) {
   this.lastName = lastName;
   this.email = email;
   this.id = id;
-  this.skills = [];
+  this.skills = skills;
 
-  this.editBasicInfo = (name = this.name, lastName = this.lastName, email = this.email) => {
+  this.basicInfoEdit = (name = this.name, lastName = this.lastName, email = this.email) => {
     this.name = (name === '' || name === null) ? this.name : name;
     this.lastName = (lastName === '' || lastName === null) ? this.lastName : lastName;
     this.email = (email === '' || email === null) ? this.email : email;
   };
 
-  this.addSkill = function(newSkill, value) {
+  this.skillAdd = function(newSkill, value) {
     this.skills = {
       ...this.skills,
       [newSkill]: value,
     }
+    saveLocalStorage(employees);
   };
 
-  this.removeSkill = function(skillToRemove) {
+  this.skillRemove = function(skillToRemove) {
     delete this.skills[skillToRemove];
+    saveLocalStorage(employees);
   };
 
-  this.modifySkill = function(skill, newValue) {
+  this.skillModify = function(skill, newValue) {
     this.skills = {
       ...this.skills,
       [skill]: newValue,
     }
+    saveLocalStorage(employees);
   };
 
   Object.defineProperty(this, 'salary', {
     get: function() {
       return salary;
     },
-    set: function(data) {
-      this.salary = {
-        ...this.salary,
-        ...data,
+    set: function(value) {
+      //value validation
+      salary = {
+        ...salary,
+        ...value,
       };
     }
   });
 }
 
-const addEmployee = (id, name, lastName, email) => {
-  const newEmployee = new Employee(id, name, lastName, email);
+const addEmployee = (id, name, lastName, email, skills) => {
+  const newEmployee = new Employee(id, name, lastName, email, skills);
   employees = {
     ...employees,
     [id]: newEmployee,
@@ -63,7 +67,7 @@ const addEmployee = (id, name, lastName, email) => {
 const editEmployee = (id, name = this.name, lastName = this.lastName, email = this.email) => {
   if(!id)
     throw new Error('id is mandatory');
-  employees[id].editBasicInfo(name, lastName, email);
+  employees[id].basicInfoEdit(name, lastName, email);
   saveLocalStorage(employees);
 };
 
@@ -85,7 +89,7 @@ const saveLocalStorage = (data) => {
 (function() {
   let employeesRetrievedData = JSON.parse(localStorage.getItem('employees')) || [];
   employeesRetrievedData.forEach((element) => {
-    addEmployee(element['id'], element['name'], element['lastName'], element['email']);
+    addEmployee(element['id'], element['name'], element['lastName'], element['email'], element['skills']);
   });
 })();
 
